aeb10fe7f31cd205feac99430f0b27be595ac3da
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2023-05-15 18:41:30 -0300

    Persistencia de dados parte 1

--- App.js
+++ App.js
@@ -1,10 +1,31 @@
-import React, {useState} from 'react';
-import {StyleSheet, Text, View} from 'react-native';
+import React, {useState, useEffect} from 'react';
+import {Button, StyleSheet, Text, View} from 'react-native';
 
 import {TextInputMask} from 'react-native-masked-text';
 
+import AsyncStorage from '@react-native-async-storage/async-storage';
+
 const App = () => {
   const [telefone, setTelefone] = useState('');
+
+  useEffect(() => {
+    AsyncStorage.getItem('@telefone')
+      .then(res => {
+        setTelefone(res);
+      })
+      .catch(err => {
+        alert('Erro ao recuperar: ' + err);
+      });
+  }, []);
+  const salvar = async function () {
+    AsyncStorage.setItem('@telefone', telefone)
+      .then(res => {
+        alert('ok');
+      })
+      .catch(error => {
+        alert('Error ao salvar: ' + error);
+      });
+  };
 
   return (
     <View>
@@ -22,6 +43,7 @@ const App = () => {
         onChangeText={txt => setTelefone(txt)}
       />
       <Text>{telefone}</Text>
+      <Button title="Salvar" onPress={salvar} />
     </View>
   );
 };

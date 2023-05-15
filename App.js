c0daef9f6dd85860400d026b659540db2216e824
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2023-05-14 23:27:53 -0300

    Initial Commit

---
+++ App.js
@@ -0,0 +1,34 @@
+import React, {useState} from 'react';
+import {StyleSheet, Text, View} from 'react-native';
+
+import {TextInputMask} from 'react-native-masked-text';
+
+const App = () => {
+  const [telefone, setTelefone] = useState('');
+
+  return (
+    <View>
+      <Text>Digite seu telefone</Text>
+
+      <TextInputMask
+        type={'cel-phone'}
+        style={styles.input}
+        options={{
+          maskType: 'BRL',
+          withDDD: true,
+          dddMask: '(99) ',
+        }}
+        value={telefone}
+        onChangeText={txt => setTelefone(txt)}
+      />
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  input: {
+    borderWidth: 1,
+  },
+});
+
+export default App;

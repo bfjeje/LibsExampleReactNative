63fdefb1b9a4d4df6aaddc7489dbd7d318959f54
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2023-05-15 18:56:09 -0300

    Persistencia de dados parte 2

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, {useState, useEffect} from 'react';
-import {Button, StyleSheet, Text, View} from 'react-native';
+import {Button, StyleSheet, Text, TextInput, View} from 'react-native';
 
 import {TextInputMask} from 'react-native-masked-text';
 
@@ -8,17 +8,25 @@ import AsyncStorage from '@react-native-async-storage/async-storage';
 const App = () => {
   const [telefone, setTelefone] = useState('');
 
+  const [name, setName] = useState('');
   useEffect(() => {
     AsyncStorage.getItem('@telefone')
       .then(res => {
-        setTelefone(res);
+        const temp = JSON.parse(res);
+        setTelefone(temp.telefone);
+        setName(temp.name);
       })
       .catch(err => {
         alert('Erro ao recuperar: ' + err);
       });
   }, []);
   const salvar = async function () {
-    AsyncStorage.setItem('@telefone', telefone)
+    const user = {
+      telefone: telefone,
+      name: name,
+    };
+
+    AsyncStorage.setItem('@telefone', JSON.stringify(user))
       .then(res => {
         alert('ok');
       })
@@ -26,10 +34,26 @@ const App = () => {
         alert('Error ao salvar: ' + error);
       });
   };
+  const apagar = function () {
+    AsyncStorage.removeItem('@telefone')
+      .then(res => {
+        alert('Apagado com success');
+      })
+      .catch(erro => {
+        alert('Erro ao apagar ' + erro);
+      });
+  };
 
   return (
     <View>
-      <Text>Digite seu telefone</Text>
+      <Text>Digite seu nome:</Text>
+      <TextInput
+        style={styles.input}
+        value={name}
+        onChangeText={txt => setName(txt)}
+      />
+
+      <Text>Digite seu telefone:</Text>
 
       <TextInputMask
         type={'cel-phone'}
@@ -44,6 +68,7 @@ const App = () => {
       />
       <Text>{telefone}</Text>
       <Button title="Salvar" onPress={salvar} />
+      <Button title="Apagar" onPress={apagar} />
     </View>
   );
 };

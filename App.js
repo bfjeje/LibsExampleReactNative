d92917f5cfa533a267127cb7d7a7340b7c2bd163
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2023-05-19 21:55:04 -0300

    Added bottom-navigation

--- App.js
+++ App.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
 
 import Tela1 from './src/Tela1';
 import Tela2 from './src/Tela2';
+import Tela3 from './src/Tela3';
 
-const Stack = createStackNavigator();
+const Stack = createBottomTabNavigator();
 
 // O projeto anterior esta em App.js.backup.
 function MyApp() {
@@ -14,6 +15,7 @@ function MyApp() {
       <Stack.Navigator>
         <Stack.Screen component={Tela1} name="Tela1" />
         <Stack.Screen component={Tela2} name="Tela2" />
+        <Stack.Screen component={Tela3} name="Tela3" />
       </Stack.Navigator>
     </NavigationContainer>
   );
---
+++ src/Tela3.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import {Text, View} from 'react-native';
+
+const Tela3 = () => {
+  return (
+    <View>
+      <Text>Tela 3</Text>
+    </View>
+  );
+};
+
+export default Tela3;

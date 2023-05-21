7b6265c416e5f4fbbb9cd00c8e50935ed629a0ad
Bruno Ellerbach <ingbrunofernandez@gmail.com>
2023-05-21 16:25:12 -0300

    Problema com drawer

--- App.js
+++ App.js
@@ -1,11 +1,13 @@
+import 'react-native-gesture-handler';
 import React from 'react';
 import {NavigationContainer} from '@react-navigation/native';
-import {createStackNavigator} from '@react-navigation/stack';
+// import {createStackNavigator} from '@react-navigation/stack';
 
 import Tela1 from './src/Tela1';
 import Tela2 from './src/Tela2';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 
-const StackNav = createStackNavigator();
+const StackNav = createDrawerNavigator();
 
 // TODO: IMPORTANTE: Ver outras coisas nos commits do Git
 
